d374f46f258c6123bb3cfc87bc0dc28f2a7496a8
Niels Bik <nielsrowinbik@gmail.com>
2017-11-13 14:12:55 +0100

    Improve build

---
+++ scripts/copy-files.js
@@ -0,0 +1,34 @@
+import path from 'path';
+import fs from 'fs-extra';
+
+const copyFile = async (file) => {
+	let buildPath = path.resolve(__dirname, '../build/', path.basename(file));
+
+	await fs.copy(file, buildPath, (err) => {
+		if (err) throw err;
+	});
+
+	return;
+};
+
+const createPackage = async () => {
+	let data = await fs.readFile(path.resolve(__dirname, '../package.json'), 'utf8');
+
+	let packageData = JSON.parse(data),
+		buildPackage = {
+			...packageData,
+			name: 'material-styled-components',
+			main: './index.js',
+			private: false
+		},
+		buildPath = path.resolve(__dirname, '../build/package.json'),
+		buildPackageData = JSON.stringify(buildPackage, null, 2);
+
+	await fs.writeFile(buildPath, buildPackageData);
+
+	return;
+};
+
+const files = ['README.md', 'CONTRIBUTING.md', 'LICENSE'];
+
+Promise.all(files.map(file => copyFile(file))).then(() => createPackage());
