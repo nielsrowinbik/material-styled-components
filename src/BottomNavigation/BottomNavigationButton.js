73492d71e59ac795bece0c4b67645cd72ad67145
Niels Bik <nielsrowinbik@gmail.com>
2017-11-15 20:29:58 +0100

    BottomNavigation: Pass a component prop to use BottomNavigationButton as a link

--- src/BottomNavigation/BottomNavigationButton.js
+++ src/BottomNavigation/BottomNavigationButton.js
@@ -1,13 +1,11 @@
-import React from 'react';
+import { createElement } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { font } from '../mixins/typography';
 
-const BottomNavigationButtonComponent = ({ className, icon, label }) => (
-	<div className={className}>
-		<label>{ label }</label>
-		{ icon }
-	</div>
+const BottomNavigationButtonComponent = ({ component, children, ...props }) => createElement(
+	component,
+	{ ...props },
+	children
 );
 
 const BottomNavigationButton = styled(BottomNavigationButtonComponent)`
@@ -18,22 +16,12 @@ const BottomNavigationButton = styled(BottomNavigationButtonComponent)`
 	box-sizing: border-box;
 	padding: 9px 12px 10px 12px;
 	display: flex;
-	flex-direction: column-reverse;
+	flex-direction: column;
 	flex-wrap: nowrap;
 	align-items: center;
 	justify-content: center;
 	color: ${props => props.theme.textColors.icon};
-	
-	& > label {
-		${props => font(400, 12) }
-		transition: font 225ms ease;
-		pointer-events: none;
-	}
-	
-	& > svg {
-		fill: currentColor;
-		flex: 0 0 24px;
-	}
+	text-decoration: none;
 
 	&:hover {
 		cursor: pointer;
@@ -46,18 +34,19 @@ const BottomNavigationButton = styled(BottomNavigationButtonComponent)`
 		flex: 0 0 72px;
 		border-radius: 50%;
 		padding: 0;
-
-		& > label {
-			${ font(400, 10) }
-			margin-top: 6px;
-			transition: none;
-		}
 	}
 `;
 
 BottomNavigationButton.propTypes = {
-	label: PropTypes.string.isRequired,
-	icon: PropTypes.node.isRequired
+	children: PropTypes.arrayOf(PropTypes.element),
+	component: PropTypes.oneOfType([
+		PropTypes.string,
+		PropTypes.element
+	])
+};
+
+BottomNavigationButton.defaultProps = {
+	component: 'div'
 };
 
 BottomNavigationButton.displayName = 'BottomNavigationButton';
